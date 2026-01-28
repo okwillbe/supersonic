762c3506b6ea6f106949e167270c5f04b7d938b6
okwillbe <804330827@qq.com>
2026-01-28 11:31:29 +0800

    ChatParseReq添加注释

--- chat/api/src/main/java/com/tencent/supersonic/chat/api/pojo/request/ChatParseReq.java
+++ chat/api/src/main/java/com/tencent/supersonic/chat/api/pojo/request/ChatParseReq.java
@@ -13,14 +13,44 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class ChatParseReq {
+    /*
+     * 用户输入的问题
+     */
     private String queryText;
+    /*
+     * 对话id
+     */
     private Integer chatId;
+    /*
+     * 助手id
+     */
     private Integer agentId;
+    /*
+     * 数据集id
+     */
     private Long dataSetId;
+    /*
+     * 用户信息
+     */
     private User user;
+    /*
+     * 过滤条件
+     */
     private QueryFilters queryFilters;
+    /*
+     * 是否保存回答
+     */
     private boolean saveAnswer = true;
+    /*
+     * 是否禁用大模型
+     */
     private boolean disableLLM = false;
+    /*
+     * 查询id
+     */
     private Long queryId;
+    /*
+     * 选中的解析
+     */
     private SemanticParseInfo selectedParse;
 }
